3659aa02d6f9510b40d0d397f9da375662097457
ilhan <cihanilhan.171@gmail.com>
2023-01-26 11:45:54 +0200

    push

--- pages/index.js
+++ pages/index.js
@@ -15,23 +15,7 @@ export default function Home({ posts }) {
   return (
     <div>
         <Layout>
-            <div style={{ marginTop: '15px',padding: '20px', backgroundColor: 'rgba(191, 191, 191, 0.15)' , borderRadius: '10px', marginBottom: '40px' }}>
-                <h1 >
-                    Hello World
-                    <img style={{marginLeft: '10px'}} width="50" height="50" src="https://camo.githubusercontent.com/66e00b1e8f7047726770f791fdd5deac5341bf0b598f6d0202cbc94cda80fb06/68747470733a2f2f692e696d6775722e636f6d2f6c73697a67476c2e676966"  alt='hello'/>
-                </h1>
-                <h4 style={{marginLeft: '15px', marginTop: '10px'}} >I post my blogs here 📫</h4>
-            </div>
 
-            <Row className="mb-5"  >
-            {posts.map((post , index) => (
-
-                    <Col  key={index}  md="3">
-                        <Post post={post} />
-                    </Col>
-
-            ))}
-            </Row>
 
             <br/>
             <div style={{textAlign: "center"}}>
---
+++ posts/php_variables.md
@@ -0,0 +1,24 @@
+---
+title: 'Variables in PHP'
+date: 'Aug 23, 2022'
+excerpt: 'Dive into variables in PHP'
+cover_image: '/images/posts/img5.jpg'
+category: 'PHP'
+category_image: '/images/languages/php.png'
+---
+
+First of all, to create a variable, a dollar sign ($) is added before the variable name in php.
+
+Basic variable creation rules are:
+- No spaces are allowed in the variable name.
+- Variable names cannot start with numbers or special characters, they must start with a letter.
+- Functions used by the programming language cannot be used as variable names.
+- The variable name must not exceed 255 characters.
+- Characters other than letters, numbers and underscores should not be used in the variable name.
+
+Example:
+ ```php
+    $num = 5;
+    $text = "cihanilhan.dev";
+    $character = 'M';
+```
